7412357e11b5fffcb7cc7308d464298c1d6c113d
S-Docker <steven.docker@hotmail.co.uk>
2020-09-16 14:31:37 +0100

    Add Stun Chance Mod
    Added final generic mod type

---
+++ Modular Weapon System/Assets/Scripts/Entities/Guns/Base Classes/Gun Modifiers/StunChanceMod.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Stun Chance Modifier", menuName = "Gun Mods/Generic/StunChanceModifier")]
+public class StunChanceMod : GunModifier
+{
+    [Tooltip("Crit chance as a percentage in value form before modifiers.")]
+    [SerializeField] int stunChanceValue;
+
+    int StunChance(int currentValue){
+        return currentValue + stunChanceValue;
+    }
+
+    public override void ApplyTo(Gun target){
+        target.GunData.StunChance.AddMod(this.GetInstanceID(), StunChance);
+    }
+
+    public override void RemoveFrom(Gun target){
+        target.GunData.StunChance.RemoveMod(this.GetInstanceID());
+    }
+}
