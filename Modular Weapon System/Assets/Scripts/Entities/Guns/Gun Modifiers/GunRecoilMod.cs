6fdade3038d25d06624a878d61896e33d2583ae0
S-Docker <steven.docker@hotmail.co.uk>
2020-10-01 16:25:03 +0100

    Refactor and implement adjustable recoil speed
    Refactored fields and methods to better indicate their use case.
    
    Implemented recoilSpeed variable for controlling how quickly lerp from current rotation to target rotation happens.
    
    Tooltips added for serialized fields.
    
    Removed unnecessary parameters from ResetTransform()

--- Modular Weapon System/Assets/Scripts/Entities/Guns/Gun Modifiers/GunRecoilMod.cs
+++ Modular Weapon System/Assets/Scripts/Entities/Guns/Gun Modifiers/GunRecoilMod.cs
@@ -3,26 +3,34 @@
 [CreateAssetMenu(fileName = "New Gun Recoil Modifier", menuName = "Gun Mods/Generic/Gun Recoil modifier")]
 public class GunRecoilMod : GunModifier
 {
+    [Header("Transform information")]
     Transform gunHolderTransform;
-    
     Quaternion originalRotation; // default gun rotation
     
+    [Header("Quaternions used for recoil calculations")]
     Quaternion startingRotation; // Rotation before applying new recoil
     Quaternion targetRotation; // Target rotation after full recoil application
     Quaternion currentRotation; // rotation after each lerped recoil application
     
+    [Header("Rotation Reset Timers")]
+    [Tooltip("Wait time before transform resets back to original rotation after player stops shooting.")]
+    [SerializeField] float transformResetDelay;
+    [Tooltip("Total time in seconds taken to reset gun from current rotation to its original rotation.")]
+    [SerializeField] float transformResetDuration;
     float elapsedTimeSinceShot;
-    [SerializeField] float waitDurationBeforeReset;
+    float elapsedTimeSinceResetStart;
 
-    float elapsedTimeSinceStartReset;
-    [SerializeField] float maxTimeToResetTransform;
-
+    [Header("Recoil Settings")]
+    [Tooltip("Horizontal recoil amount in degrees.")]
     [SerializeField] float horizontalRecoilAmount;
+    [Tooltip("Vertical recoil amount in degrees.")]
     [SerializeField] float verticalRecoilAmount;
+    [Tooltip("How quick the gun goes from current rotation to target rotation after shooting.")]
+    [SerializeField] float recoilSpeed;
 
-    void AddRecoil(Gun target){
+    void CalculateRecoil(Gun target){
         elapsedTimeSinceShot = 0;
-        elapsedTimeSinceStartReset = 0;
+        elapsedTimeSinceResetStart = 0;
 
         startingRotation = gunHolderTransform.rotation;
         Vector3 targetRecoilAmount = new Vector3(-horizontalRecoilAmount, Random.Range(-verticalRecoilAmount, verticalRecoilAmount), 0);
@@ -31,9 +39,8 @@ public class GunRecoilMod : GunModifier
 
     void OnUpdate(Gun target){
         // if still in recoil stage
-        if (elapsedTimeSinceShot < waitDurationBeforeReset){
-
-            gunHolderTransform.rotation = Quaternion.Lerp(startingRotation, targetRotation, elapsedTimeSinceShot);
+        if (elapsedTimeSinceShot < transformResetDelay){
+            gunHolderTransform.rotation = Quaternion.Lerp(startingRotation, targetRotation, recoilSpeed * elapsedTimeSinceShot);
 
             currentRotation = gunHolderTransform.rotation;
             elapsedTimeSinceShot += Time.deltaTime;
@@ -41,36 +48,34 @@ public class GunRecoilMod : GunModifier
         else{
             if (currentRotation == originalRotation) return;
 
-            ResetTransform(target, maxTimeToResetTransform);
+            ResetTransform();
         }
     }
 
-    void ResetTransform(Gun target, float timeTakenToReset){
-        elapsedTimeSinceStartReset += Time.deltaTime;
+    void ResetTransform(){
+        elapsedTimeSinceResetStart += Time.deltaTime;
 
-        if (elapsedTimeSinceStartReset < timeTakenToReset){
-            Debug.Log("rotate here");
-            float rotationProgress = elapsedTimeSinceStartReset / Mathf.Min(timeTakenToReset);
+        if (elapsedTimeSinceResetStart < transformResetDuration){
+            float rotationProgress = elapsedTimeSinceResetStart / transformResetDuration;
             
             gunHolderTransform.rotation = Quaternion.Slerp(currentRotation, originalRotation, rotationProgress);
         }
     }
-    
-    
+
     public override void ApplyTo(Gun target){
         gunHolderTransform = target.transform.parent;
 
         originalRotation = gunHolderTransform.rotation;
         currentRotation = originalRotation;
-        elapsedTimeSinceShot = waitDurationBeforeReset;
+        elapsedTimeSinceShot = transformResetDelay;
         
         
-        target.FireComponent.onFire += AddRecoil;
+        target.FireComponent.onFire += CalculateRecoil;
         target.onUpdate += OnUpdate;
     }
 
     public override void RemoveFrom(Gun target){
-        target.FireComponent.onFire -= AddRecoil;
+        target.FireComponent.onFire -= CalculateRecoil;
         target.onUpdate -= OnUpdate;
     }
 }
