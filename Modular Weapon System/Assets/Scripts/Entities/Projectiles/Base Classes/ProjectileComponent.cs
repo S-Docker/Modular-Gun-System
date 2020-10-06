bccc11d01f426e497fce0335648fa3cd431c7f8a
S-Docker <steven.docker@hotmail.co.uk>
2020-10-06 18:43:33 +0100

    Implement effect lifetime
    Instantiated effects now destroy after duration of the particle system, will require future refactor to avoid GetComponent usage.

--- Modular Weapon System/Assets/Scripts/Entities/Projectiles/Base Classes/ProjectileComponent.cs
+++ Modular Weapon System/Assets/Scripts/Entities/Projectiles/Base Classes/ProjectileComponent.cs
@@ -5,10 +5,11 @@ public abstract class ProjectileComponent : MonoBehaviour
     [Header("Projectile Effect Settings")]
     [SerializeField] protected bool hasEffect = false;
     [SerializeField] GameObject effect = null;
-    
+
     protected void InitialiseEffect(Vector3 position){
         GameObject effectObject = Instantiate(effect, position, Quaternion.identity);
+        float effectLifetime = effectObject.GetComponent<ParticleSystem>().main.duration;
         
-        Destroy(effectObject, 2f);
+        Destroy(effectObject, effectLifetime);
     }
 }
