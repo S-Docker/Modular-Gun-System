4e58d27a2f8bd3dd419d308a9f786f81cb5b4bea
S-Docker <steven.docker@hotmail.co.uk>
2020-10-13 20:29:24 +0100

    Add bullet spread modifier support and fix edge cases
    Bullet spread modifier support has been added.
    
    New generic bullet spread mod has been created.
    
    Mathf.Max used when getting values of non percentage based modifiable attributes to ensure returned value is not below the minimum threshold for that stat type.

--- Modular Weapon System/Assets/Scripts/Entities/Projectiles/ProjectileHitscanComponent.cs
+++ Modular Weapon System/Assets/Scripts/Entities/Projectiles/ProjectileHitscanComponent.cs
@@ -3,6 +3,9 @@
 [DisallowMultipleComponent]
 public class ProjectileHitscanComponent : ProjectileComponent
 {
+    [Range(1, 100)]
+    [SerializeField] float maxHitscanDistance = default; public float MaxHitscanDistance => maxHitscanDistance;
+    
     public void InitialiseHitscan(Vector3 pointOfImpact){
         transform.position = pointOfImpact;
     }
