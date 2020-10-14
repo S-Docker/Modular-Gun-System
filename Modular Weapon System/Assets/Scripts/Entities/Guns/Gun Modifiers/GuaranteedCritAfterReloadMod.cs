866ca6c43bd328ec5aff565c10162798b5b05f9f
S-Docker <steven.docker@hotmail.co.uk>
2020-10-14 01:17:24 +0100

    Restructure modifier folders
    Moved generic modifiers into a separate 'Generic' folder

--- Modular Weapon System/Assets/Scripts/Entities/Guns/Gun Modifiers/GuaranteedCritAfterReloadMod.cs
+++ Modular Weapon System/Assets/Scripts/Entities/Guns/Gun Modifiers/GuaranteedCritAfterReloadMod.cs
@@ -1,13 +1,13 @@
 ﻿using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Guaranteed Crit After Reload", menuName = "Gun Mods/Guaranteed Crit After Reload")]
+[CreateAssetMenu(fileName = "New Guaranteed Crit After Reload", menuName = "Gun Mods/Guaranteed Crit(s) After Reload")]
 public class GuaranteedCritAfterReloadMod : GunModifier
 {
     int bulletCount;
     [Tooltip("Maximum number of bullets that can crit after reload.")]
-    [SerializeField] int maxCritBullets;
+    [SerializeField] int maxCrits;
     
-    bool CanCrit() => bulletCount < maxCritBullets;
+    bool CanCrit() => bulletCount < maxCrits;
 
     int IncreaseCritChancePercentage(int critChance){
         if (!CanCrit()){ return critChance; }
@@ -25,7 +25,7 @@ public class GuaranteedCritAfterReloadMod : GunModifier
 
     public override void ApplyTo(Gun target){
         // Stops crits being applied prior to first reload
-        bulletCount = maxCritBullets;
+        bulletCount = maxCrits;
 
         target.FireComponent.onFire += OnFire;
         target.ReloadComponent.onReload += OnReload;
